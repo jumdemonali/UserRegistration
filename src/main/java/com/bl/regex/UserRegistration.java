65d47a2fa62a47e2c5e8f159b97143d7d0fd93b8
jumdemonali <jumdemonali@gmail.com>
2021-09-14 19:42:06 +0530

    Added: Validation of first name

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -1,7 +1,18 @@
 package com.bl.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome to user registration!!");
+        String input = "Mona";
+        String reqex = "^[A-Z][a-z]{2,}$";
+        Pattern pattern = Pattern.compile(reqex);
+        Matcher matcher=pattern.matcher(input);
+        if(matcher.matches()) {
+            System.out.println("Valid");
+        }else{
+            System.out.println("Invalid");
+        }
     }
 }
