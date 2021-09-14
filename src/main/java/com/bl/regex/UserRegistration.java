3be8ae02f5a1f946f3210d2ae506d5f370769816
jumdemonali <jumdemonali@gmail.com>
2021-09-14 20:25:55 +0530

    Added: Validation of email

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -5,29 +5,15 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter first name");
-            String firstName = scan.nextLine();
-            System.out.println("Enter last name");
-            String lastName = scan.nextLine();
-            UserRegistration userRegistration = new UserRegistration();
-            userRegistration.firstNameValidation(firstName);
-            userRegistration.lastNameValidation(lastName);
+        String input = "abc.xyz@gmail.com.in";
+        String regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}(.[a-z]{2}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
         }
 
-
-    private void firstNameValidation(String firstName) {
-        String regex = "^[A-Z][a-z]{2,}";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(firstName);
-        System.out.println("Is valid: " + matcher.matches());
     }
-    private void lastNameValidation(String lastName) {
-        String regex = "^[A-Z][a-z]{2,}";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(lastName);
-        System.out.println("Is valid: " + matcher.matches());
-    }
-
 }
-
